d7f5ad56d8d083fa1bd7836d23dc46ef710ae42c
akrystian <akrystian@gmail.com>
2017-01-08 18:45:17 +0100

    docker

---
+++ docker/setEnv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/mysql
+export SPRING_DATASOURCE_USERNAME=mysql
+export SPRING_DATASOURCE_PASSWORD=mysql
+export SPRING_DATASOURCE_DRIVER=com.mysql.jdbc.Driver
