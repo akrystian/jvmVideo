540fc1f857f6fcdb54ed1c05e84cbd64f9d6288e
akrystian <akrystian@gmail.com>
2017-05-03 22:56:13 +0200

    https://github.com/akrystian/jvmVideo/issues/4 - small fix

---
+++ src/test/java/pro/adamski/jvmvideo/entity/VideoTest.java
@@ -0,0 +1,45 @@
+package pro.adamski.jvmvideo.entity;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.time.Duration;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author akrystian.
+ */
+public class VideoTest {
+
+    private YouTubeChannel channel = new YouTubeChannel("name", new DateTime(DateTime.now()),
+            "identifier");
+    private Video videoA = new Video(
+            "id1",
+            "title2",
+            "description2",
+            new Date(0L),
+            Duration.ofMinutes(552),
+            "https://i.ytimg.com/vi/zQll41ha5_g/default.jpg", channel);
+    private Video videoB = new Video(
+            "id1",
+            "title2",
+            "description1",
+            new Date(0L),
+            Duration.ofMinutes(552),
+            "https://i.ytimg.com/vi/zQll41ha5_g/default.jpg", channel);
+
+    @Test
+    public void shouldReturnFields() throws Exception {
+        //that
+        assertThat(videoA.getId(), is(0L));
+        assertThat(videoA.getTitle(), is("title2"));
+        assertThat(videoA.getDescription(), is("description2"));
+        assertThat(videoA.getPublishDate(), is(new Date(0L)));
+        assertThat(videoA.getDuration(), is(Duration.ofMinutes(552)));
+        assertThat(videoA.getThumbnailLink(), is("https://i.ytimg.com/vi/zQll41ha5_g/default.jpg"));
+        assertThat(videoA.getSource(), is(channel));
+    }
+}
