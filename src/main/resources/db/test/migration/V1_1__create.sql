4753b8a9b6e00c3bd9c321a9ac1659083c46323d
akrystian <akrystian@gmail.com>
2017-09-09 22:41:03 +0200

    https://github.com/akrystian/jvmVideo/issues/10 - Workaround to the problem with setting utf8mb4 charset on H2 DB.

---
+++ src/main/resources/db/test/migration/V1_1__create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `video` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `video_id` varchar(255) DEFAULT NULL,
+  `description` varchar(2024) DEFAULT NULL,
+  `duration` tinyblob,
+  `publish_date` date NOT NULL,
+  `thumbnail_link` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
---
+++ src/main/resources/db/test/migration/V1_3__sources.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `video`
+  MODIFY COLUMN `duration` long DEFAULT NULL;
+
+
