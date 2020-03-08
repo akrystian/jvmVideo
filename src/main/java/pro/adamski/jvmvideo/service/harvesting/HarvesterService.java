47da6fd555320f334fda9ac26cf47a3e31474a55
akrystian <akrystian@gmail.com>
2020-03-08 22:54:27 +0100

    fix for exception handling

--- src/main/java/pro/adamski/jvmvideo/service/harvesting/HarvesterService.java
+++ src/main/java/pro/adamski/jvmvideo/service/harvesting/HarvesterService.java
@@ -66,7 +66,7 @@ public class HarvesterService {
     private void updateSingleVideoStats(Video video) {
         try {
             videoRepository.save(youTubeService.updateStats(video));
-        } catch (IOException | HarvestingException e) {
+        } catch (Exception e) {
             log.error("Problem with update stats for videoId: " + video.getVideoId() +  "videoLink"+ video
                     .getVideoLink() , e);
         }
@@ -88,8 +88,8 @@ public class HarvesterService {
                 harvestSingleVideo(channel, videoId);
                 harvestedVideosCounter++;
             }
-        } catch (IOException e) {
-            throw new HarvestingException(e);
+        } catch (Exception e) {
+            log.error("Problem with harvesting: ", e);
         }
         return harvestedVideosCounter;
     }
